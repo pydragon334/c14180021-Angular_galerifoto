a5b28015cce82452fec47687259b56e5fb9740a7
pydragon334 <gavriel.ev@gmail.com>
2021-03-16 22:24:44 +0700

    update latihan

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,5 +8,29 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+  
+  tebakan = "0";
+  jawaban = "";
+  hasil = "";
+  image = "./assets/mmm.jpg";
 
+  Hasil(){
+    var getRandNmb = function(long) {
+      var charsBeingRandomized = "0123456789";
+      var returnNumb = "";
+      for (var x = 0; x < long; x++) {
+          var i = Math.floor(Math.random() * charsBeingRandomized.length);
+          returnNumb += charsBeingRandomized.charAt(i);
+      }
+      return returnNumb;
+    }
+    this.tebakan = getRandNmb(1);
+    if(this.tebakan == this.jawaban){
+      this.hasil = "benar";
+      this.image ="./assets/ok.jpg";
+    } else {
+      this.hasil = "salah";
+      this.image ="./assets/ugh.gif";
+    }
+  }
 }
